54e945f6e71e319ea084f7b8b27de0bff43ebec3
ZakariaHn <56868939+ZakariaHn@users.noreply.github.com>
2020-02-22 21:42:43 +0100

    Update validation1.js

--- validation1.js
+++ validation1.js
@@ -1,16 +1,15 @@
 const validPin1 = pinCode => {
-  let strArr = pinCode.split("");
-  let num = strArr.map(digit => parseInt(digit));
-  let sum = num.reduce((a, b) => a + b);
+  let strArr = pinCode.split(""),
+    num = strArr.map(digit => parseInt(digit)),
+    sum = num.reduce((a, b) => a + b);
   for (let i of num) {
     diff = num[i + 1] == num[0] ? false : true;
+    result =
+      sum > 5 &&
+      !isNaN(pinCode) &&
+      pinCode.length == 4 &&
+      pinCode[3] % 2 == 0 &&
+      diff;
   }
-  return (
-    sum > 5 &&
-    !isNaN(pinCode) &&
-    pinCode.length == 4 &&
-    pinCode[3] % 2 == 0 &&
-    diff
-  );
+  return result;
 };
-validPin1();
